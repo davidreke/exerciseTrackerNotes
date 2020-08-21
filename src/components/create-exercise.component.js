7b7cfd90f299d98ae4af7b999271f87aa9027864
davidreke <54542172+davidreke@users.noreply.github.com>
2020-08-20 21:45:42 -0500

    Thursday night commits

---
+++ src/components/create-exercise.component.js
@@ -0,0 +1,146 @@
+import React, { Component } from "react";
+import DatePicker from 'react-datepicker'
+import "react-datepicker/dist/react-datepicker.css"
+import axios from 'axios'
+
+export default class CreateExercise extends Component {
+  constructor(props) {
+    super(props);
+
+    // we need these so we can use this in a function without making it an arrow function
+      this.onChangeUsername = this.onChangeUsername.bind(this);
+      this.onChangeDescription = this.onChangeDescription.bind(this);
+      this.onChangeDuration = this.onChangeDuration.bind(this);
+      this.onChangeDate = this.onChangeDate.bind(this);
+      this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      username: "",
+      description: "",
+      duration: 0,
+      date: new Date(),
+      users: [],
+    };
+  }
+      // we use this to hard code in a single user until we bring in the databse
+      componentDidMount() {
+      // this.setState({ 
+      //   users: ['test user'],
+      //   username: 'test user'
+      // });
+      axios.get('http://localhost:5000/users/')
+        .then(response =>{
+          if (response.data.length > 0){
+            this.setState({
+              users: response.data.map(user => user.username),
+              // returns the user from each artcile in the db
+              username: response.data[0].username
+            })
+          }
+        })
+        // once you have this get, look at the create exercise user drop down
+    }
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value,
+    });
+  }
+
+  onChangeDescription(e) {
+    this.setState({
+      description: e.target.value,
+    });
+  }
+
+  onChangeDuration(e) {
+    this.setState({
+      duration: e.target.value,
+    });
+  }
+
+  onChangeDate(date) {
+    this.setState({
+      date: date,
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    const exercise = {
+      username: this.state.username,
+      description: this.state.description,
+      duration: this.state.duration,
+      date: this.state.date,
+    };
+    // we console.log exercise b/c we are going to to do something different later, where we submit this to the database.
+    console.log(exercise);
+    
+    axios.post('http://localhost:5000/exercises/add' ,exercise)
+    window.location = "/";
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Create New Exercise Log</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Username: </label>
+            <select
+              ref="userInput"
+              required
+              className="form-control"
+              value={this.state.username}
+              onChange={this.onChangeUsername}
+            >
+              {this.state.users.map(function (user) {
+                return (
+                  <option key={user} value={user}>
+                    {user}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Description: </label>
+            <input
+              type="text"
+              required
+              className="form-control"
+              value={this.state.description}
+              onChange={this.onChangeDescription}
+            />
+          </div>
+          <div className="form-group">
+            <label>Duration (in minutes): </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.duration}
+              onChange={this.onChangeDuration}
+            />
+          </div>
+          <div className="form-group">
+            <label>Date: </label>
+            <div>
+              <DatePicker
+                selected={this.state.date}
+                onChange={this.onChangeDate}
+              />
+            </div>
+          </div>
+
+          <div className="form-group">
+            <input
+              type="submit"
+              value="Create Exercise Log"
+              className="btn btn-primary"
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+  }
---
+++ src/components/navbar.component.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      // everything below is the navbar from the bootstrap documentation converted into jsx.
+      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+        <Link to="/" className="navbar-brand">
+          ExcerTracker
+        </Link>
+        <div className="collpase navbar-collapse">
+          <ul className="navbar-nav mr-auto">
+            <li className="navbar-item">
+              <Link to="/" className="nav-link">
+                Exercises
+              </Link>
+            </li>
+            <li className="navbar-item">
+              <Link to="/create" className="nav-link">
+                Create Exercise Log
+              </Link>
+            </li>
+            <li className="navbar-item">
+              <Link to="/user" className="nav-link">
+                Create User
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
